40d8aaf660fbf48a73367e8b4a014afad71f9d75
trader <trader.mathias@proton.me>
2026-02-18 13:09:36 -0500

    Fix wsgi and Procfile for Dash deployment

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,3 @@
-"""
-WSGI wrapper for Render deployment.
-Dash apps need to use app.server for gunicorn.
-"""
-
 from agentic_trust_dashboard import app
 
-# For Dash apps, gunicorn needs the Flask server object
-# Not the Dash app object
 server = app.server
-
-if __name__ == "__main__":
-    app.run_server(debug=False)
